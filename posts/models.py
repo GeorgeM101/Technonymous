eca555782a912135ded2fc506780af4a841601e5
GeorgeM101 <Georgmboya@gmail.com>
2023-04-24 16:52:28 +0300

    added value field

--- posts/models.py
+++ posts/models.py
@@ -22,3 +22,11 @@ class Comment(models.Model):
     def __str__(self):
         return self.content
     
+class Vote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    value = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.user.username}' - {self.post.title}
+    
