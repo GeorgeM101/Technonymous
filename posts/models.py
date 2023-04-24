7e024c1b29dbedad88a033df742929a3d100feb0
GeorgeM101 <Georgmboya@gmail.com>
2023-04-24 16:46:19 +0300

    added a comment field

--- posts/models.py
+++ posts/models.py
@@ -12,3 +12,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
+    
