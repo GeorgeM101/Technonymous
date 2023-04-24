422a634ed9ea07ca02d0343a35876e8f0190f6b3
GeorgeM101 <Georgmboya@gmail.com>
2023-04-24 16:42:46 +0300

    added the post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    anonymous = models.BooleanField(default=False)
+    latitude = models.DecimalField(max_digits=9,decimal_places=6, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
