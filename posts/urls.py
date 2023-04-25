30d48f268d2b90cd09a0bb349d9b956a776dfc2b
GeorgeM101 <Georgmboya@gmail.com>
2023-04-25 19:49:02 +0300

    added and updated base.html

--- posts/urls.py
+++ posts/urls.py
@@ -1,20 +1,12 @@
 from django.urls import path
-from .views import (
-    post_list_view,
-    post_detail_view,
-    post_create_view,
-    post_update_view,
-    post_delete_view,
-    comment_create_view,
-)
+from .views import post_list,post_detail,post_delete,post_create,post_update
 
 app_name = 'posts'
 
 urlpatterns = [
-    path('', post_list_view, name='post_list'),
-    path('<int:pk>/', post_detail_view, name='post_detail'),
-    path('create/', post_create_view, name='post_create'),
-    path('<int:pk>/update/', post_update_view, name='post_update'),
-    path('<int:pk>/delete/', post_delete_view, name='post_delete'),
-    path('<int:pk>/comment/', comment_create_view, name='comment_create'),
+    path('posts/', post_list, name='post_list'),
+    path('post/<int:post_id>/', post_detail, name='post_detail'),
+    path('create/', post_create, name='post_create'),
+    path('<int:pk>/update/', post_update, name='post_update'),
+    path('<int:pk>/delete/', post_delete, name='post_delete'),
 ]
