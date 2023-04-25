b22a11628296058e1efb62a36fe1f0e6c44debbb
GeorgeM101 <Georgmboya@gmail.com>
2023-04-25 12:21:10 +0300

    added comment form

--- posts/forms.py
+++ posts/forms.py
@@ -5,3 +5,8 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'content', 'image')
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
